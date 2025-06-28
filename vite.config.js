4e5e540f928e2b83f7cfd463a07df727ffbc8e6f
sweetpy <agubouy@gmail.com>
2025-06-28 10:31:22 +0300

    Fix cPanel Loading Issues

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: './', // Important for cPanel hosting
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
